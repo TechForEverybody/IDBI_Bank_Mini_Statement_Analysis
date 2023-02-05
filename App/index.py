3e04f5d44adc0dae7509936818e702cab20f1f5c
TechForEverybody <Pankajsinghat95@gmail.com>
2023-02-05 19:41:55 +0530

    Data reading is done

--- App/index.py
+++ App/index.py
@@ -1,1 +1,11 @@
-<h1 style="text-align: center;font-size: 36px;">Analysis</h1>
+from flask import Flask,render_template
+
+App=Flask(__name__)
+
+
+@App.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    App.run()
